e90fc6c498a26d315c07bff2f452c6992f786f67
RhinoFX <151397834+RhinoFX@users.noreply.github.com>
2023-11-30 15:25:59 -0800

    City Sustainability Calculator Changes as of 11/30

--- Group Project - Anika 2/City Sustainability Calculator/City Sustainability Calculator/City Sustainability Calculator.cpp
+++ Group Project - Anika 2/City Sustainability Calculator/City Sustainability Calculator/City Sustainability Calculator.cpp
@@ -10,16 +10,18 @@ get scoreing system working
 #include <iostream>
 #include <iomanip>
 #include <string>
+#include <fstream>
 #include "C:\Users\calic\Downloads\Headers.h"
 using namespace std;
 
+string scoreLine;
 void GetResponse(char& userInput) {
 
     cin >> userInput;
 
     while (userInput != 'y' && userInput != 'n') {
 
-        cout << "Invalid response. Please respond with yes or no.";
+        cout << "Invalid response. Please respond with (y) for yes or (n) for no.";
 
         cin >> userInput;
     }
@@ -29,13 +31,23 @@ int main()
 {
     float numPop;
     string cityName;
+    int buildingScore, co2Score, waterScore, airScore, energyScore, governanceScore, transportScore, wasteScore, totalScore;
+    string scoreLine;
 
-    cout << "What is the name of this city?: " << endl;
+    cout << "What is the name of this city?: ";
     getline(cin, cityName);
 
-    cout << "What is the population of this city?: " << endl;
+    cout << "What is the population of this city?: ";
     cin >> numPop;
 
+    ofstream outputFile("City Sustainability Scores.txt", ios::app);
+
+if (!outputFile.is_open()) {
+  cout << "Error opening the file!" << endl;
+  return 1; // Return an error code
+}
+
+ outputFile.close();
     RunBuildingsAssessment();
 
     runCarbonEmissionsAssessment(numPop, cityName);
@@ -52,6 +64,40 @@ int main()
 
     RunEnergyAssessment();
 
+    ifstream inputFile("City Sustainability Scores.txt");
+
+    inputFile >> buildingScore;
+
+    inputFile >> co2Score;
+
+    inputFile >> wasteScore;
+
+    inputFile >> transportScore;
+
+    inputFile >> waterScore;
+
+    inputFile >> airScore;
+
+    inputFile >> governanceScore;
+
+    inputFile >> energyScore;
+
+    totalScore = (buildingScore + co2Score + waterScore + airScore + energyScore + governanceScore + transportScore + wasteScore) / 8;
+
+    ofstream outputFileAppend("City Sustainability Scores.txt", ios::app);
+
+if (!inputFile.is_open()) {
+  cout << "Error opening the file!" << endl;
+  return 1; // Return an error code
+}
+
+cout<< "The total score for each category is as follows:" << endl;
+// Read and print each line of the file
+while (getline(inputFile, scoreLine)) {
+  cout << scoreLine << endl;
+}
+
+inputFile.close();
     return 0;
 }
 
